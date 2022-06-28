de3ecdbf8197132936b0df29b5159b2f63a69a10
Carlos <carlosmonterrey17@gmail.com>
2022-06-28 11:51:33 -0400

    Se trabajo en el metodo de autenticarse

--- src/app/services/perfil.service.ts
+++ src/app/services/perfil.service.ts
@@ -8,17 +8,23 @@ import { Observable } from 'rxjs';
 export class PerfilService {
 
   private apiUrl = 'http://localhost:5000/usuarios';
-  public usuario: Observable<Object> ;
+  public usuario: Object = {};
+
 
   constructor(private http: HttpClient) { }
 
-  login(usuario: string, contrasena: string): Observable<Object>{
+  login(usuario: string, contrasena: string): boolean{
     this.usuario = this.http.get<Object>(this.apiUrl, {
       params: {
         usuario: usuario,
         contrasena: contrasena,
       }
+    }).subscribe(data => {
+      this.usuario = data
+      if (Object.entries(this.usuario).length ===1) {
+        return true;
+      }
    });
-   return this.usuario;
+   return false;
   }
 }
